c9879c61d1ad8a4d141ae4d4648dd044a9112c5b
890 <alagoke@gmail.com>
2025-07-14 12:09:20 +0100

    testing

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -2,25 +2,42 @@ import React from "react";
 import icons from "./icons";
 
 function Navbar(props) {
-  
   return (
     <div className="bg-[#d7527d] w-full text-white text-center flex flex-col items-center font-serif text-sm font-medium py-2">
-      <p>{props.announcement}</p>
+      <p className="px-2 text-xs sm:text-sm">{props.announcement}</p>
 
-      <div className="w-full bg-white mt-3 h-16 flex items-center justify-between px-4">
-       <div className="">
-        <span className="flex gap-2 ml-10">
-          {icons.slice(0, 2).map((icon, index) => (
-            <button onClick={()=>{console.log(`${index + 1}`)}} className="text-black hover:text-yellow-300 transition duration-300 h-15 w-15" key={index}>{icon}</button>
-          ))}
-        </span>
-        </div> 
-        <img src="/image/topilogo.jpg" alt="Topi Logo" />
-        <span className="flex gap-2 mr-10">
-          {icons.slice(2,4).map((icon, index) => (
-            <button onClick={()=>{console.log(`${index+3}`)}} className="text-black hover:text-yellow-300 transition duration-300 h-15 w-15" key={index + 2}>{icon}</button>
-          ))}
-        </span>
+      <div className="w-full bg-white mt-3 h-16 flex items-center justify-between px-4 sm:px-6">
+        
+ <div className="flex items-center space-x-4 ">
+  {icons.slice(0, 2).map((icon, index) => (
+    <button
+      key={index}
+      onClick={() => console.log(`${index + 1}`)}
+      className="ml-4 text-gray-500 hover:text-yellow-300 transition duration-300 text-2xl"
+    >
+      {icon}
+    </button>
+  ))}
+</div>
+        <img
+          src="/image/topilogo.jpg"
+          alt="Topi Logo"
+          className="h-10 sm:h-12 object-contain"
+        />
+
+<div className="flex items-center space-x-4">
+  {icons.slice(2, 4).map((icon, index) => (
+    <button
+      key={index + 2}
+      onClick={() => console.log(index + 3)}
+      className="mr-4 text-gray-500 hover:text-yellow-300 transition duration-300 text-xl sm:text-2xl"
+    >
+      {icon}
+    </button>
+  ))}
+</div>
+
+
       </div>
     </div>
   );
